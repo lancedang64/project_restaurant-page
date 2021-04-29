c69c65d10fc69f007ff6e0e60a05ad8af860188b
Lance Dang <lancedang64@gmail.com>
2021-04-29 21:18:15 +1000

    Initialized webpack, src js & css, css-style-loader.

---
+++ webpack.config.js
@@ -0,0 +1,22 @@
+const path = require('path');
+
+module.exports = {
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: [
+          'style-loader',
+          'css-loader'
+        ]
+      }
+    ]
+  },
+  mode: 'production',
+  entry: './src/index.js',
+  output: {
+    filename: 'main.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+};
+
