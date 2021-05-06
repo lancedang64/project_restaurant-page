0ba4cd531ce70f2ca772b5be71499759174b234e
Lance Dang <lancedang64@gmail.com>
2021-05-06 18:37:10 +1000

    Added copyright declaration

--- src/modules/footer.js
+++ src/modules/footer.js
@@ -2,6 +2,7 @@ const footer = (parentNode) => {
   const footer = document.createElement('footer');
   const footerContent = document.createElement('span');
   const gitHubLogo = document.createElement('a');
+  const copyright = document.createElement('p');
 
   footerContent.setAttribute('class', 'footer-content');
   footerContent.innerHTML = 'Made by Lance Dang';
@@ -9,7 +10,9 @@ const footer = (parentNode) => {
   gitHubLogo.setAttribute('id', 'github-logo');
   gitHubLogo.setAttribute('href', 'https://github.com/lancedang64');
 
-  footer.append(footerContent, gitHubLogo);
+  copyright.innerHTML = `All images, logos used in this personal project belong to <a href='http://www.armorywharfcafe.com/' , id='shop'>Armory Wharf Cafe</a> <br> Pending permission!`;
+
+  footer.append(footerContent, gitHubLogo, copyright);
 
   parentNode.appendChild(footer);
 };
