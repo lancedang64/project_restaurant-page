450e916bad337cc4ef57f7b6a357e6b4b5e83be7
Lance Dang <lancedang64@gmail.com>
2021-05-17 09:47:01 +1000

    Configured webpack to dev and prod settings

--- webpack.common.js
+++ webpack.common.js
@@ -1,8 +1,9 @@
 const path = require('path');
 
 module.exports = {
-  mode: 'production',
-  entry: './src/index.js',
+  entry: {
+    app: './src/index.js',
+  },
   output: {
     filename: 'bundle.js',
     path: path.resolve(__dirname, 'dist'),
